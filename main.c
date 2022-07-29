4f901e1c10a66749b78d7eaf1eae256542cf4d8e
namelss <jeronimocano22@gmail.com>
2022-07-29 14:33:21 -0500

    add exclamation marks

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    printf("La vida es bella\n");
+    printf("La vida es bella!!!\n");
     printf("lo que tu corazón te dicte\n");
     return(EXIT_SUCCESS);
 }
